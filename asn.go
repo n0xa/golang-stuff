b647528c49694ecf742d1dd4f623ed370df8bd9e
axon <axon@chimaera.labs.h-i-r.net>
2023-01-27 23:39:04 -0600

    asn.go added

--- asn.go
+++ asn.go
@@ -1,6 +1,6 @@
 package main
-// ipasn.go - Fetches the BGP ASN(s) and descriptions 
-// for a given ip address
+// asn.go - pulls ipv4 and ipv6 prefixes from a
+// BGP Autonomous Systen Number.
 // ax0n@h-i-r.net | IG: @4x0nn | Twitter: @ax0n
 import (
 	"os"
@@ -17,22 +17,22 @@ type IpAsn struct {
 	Status        string `json:"status"`
 	StatusMessage string `json:"status_message"`
 	Data          struct {
-		IP        string `json:"ip"`
-		PtrRecord string `json:"ptr_record"`
-		Prefixes  []struct {
-			Prefix string `json:"prefix"`
-			IP     string `json:"ip"`
-			Cidr   int    `json:"cidr"`
-			Asn    struct {
-				Asn         int    `json:"asn"`
-				Name        string `json:"name"`
-				Description string `json:"description"`
-				CountryCode string `json:"country_code"`
-			} `json:"asn"`
+		Ipv4Prefixes  []struct {
+			Prefix      string `json:"prefix"`
+			IP          string `json:"ip"`
+			Cidr 	    int    `json:"cidr"`
 			Name        string `json:"name"`
 			Description string `json:"description"`
 			CountryCode string `json:"country_code"`
-		} `json:"prefixes"`
+		} `json:"ipv4_prefixes"`
+		Ipv6Prefixes  []struct {
+			Prefix      string `json:"prefix"`
+			IP          string `json:"ip"`
+			Cidr 	    int    `json:"cidr"`
+			Name        string `json:"name"`
+			Description string `json:"description"`
+			CountryCode string `json:"country_code"`
+		} `json:"ipv6_prefixes"`
 		RirAllocation struct {
 			RirName          string      `json:"rir_name"`
 			CountryCode      interface{} `json:"country_code"`
@@ -60,15 +60,14 @@ type IpAsn struct {
 	} `json:"@meta"`
 }
 
-	if len(os.Args) < 2 {
-		fmt.Println("IP Address needed")
-		os.Exit(1)
-	}
-
-    ip := os.Args[1]
-
+    if len(os.Args) < 2 {
+      fmt.Println("ASN Required")
+      os.Exit(1)
+    }
+    asn := os.Args[1]
+    
 	var ipasn IpAsn
-	resp, err := http.Get("https://api.bgpview.io/ip/" + ip)
+	resp, err := http.Get("https://api.bgpview.io/asn/" + asn + "/prefixes")
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -82,11 +81,14 @@ type IpAsn struct {
 		fmt.Println(string(body))
 		log.Panicln(err)
 	}
-	if len(ipasn.Data.Prefixes) > 0 {
+	if len(ipasn.Data.Ipv4Prefixes) > 0 {
 
-		fmt.Printf("%6s | %2s | %-10s | %-25s\n", "ASNum", "CC", "Name", "Description")
-    	for _, asn := range ipasn.Data.Prefixes {
-			fmt.Printf("%6d | %2s | %-10s | %-25s\n", asn.Asn.Asn, asn.Asn.CountryCode, asn.Asn.Name, asn.Asn.Description)
+		fmt.Printf("%6s | %2s | %-10s | %-25s\n", "Prefix", "CC", "Name", "Description")
+    	for _, asn := range ipasn.Data.Ipv4Prefixes {
+			fmt.Printf("%20s | %2s | %-20s | %-25s\n", asn.Prefix, asn.CountryCode, asn.Name, asn.Description)
+   		 }
+    	for _, asn := range ipasn.Data.Ipv6Prefixes {
+			fmt.Printf("%20s | %2s | %-20s | %-25s\n", asn.Prefix, asn.CountryCode, asn.Name, asn.Description)
    		 }
 	}else{
 		fmt.Println("BGPView Returned no ASN Prefixes.")
